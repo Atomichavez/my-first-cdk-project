8a57c86cfe92971017b99e9788b71c9a388e22f6
atomichavez <dchavez0628@gmail.com>
2020-08-24 22:48:20 -0500

    Initial commit

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     aws_iam as iam,
     aws_sqs as sqs,
     aws_sns as sns,
+    aws_s3 as s3,
     aws_sns_subscriptions as subs,
     core
 )
@@ -11,6 +12,12 @@ class MyFirstCdkProjectStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        s3.Bucket(
+            self,
+            "myBucketId"
+
+        )
 
         queue = sqs.Queue(
             self, "MyFirstCdkProjectQueue",
